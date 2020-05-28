44f5b4b6f1bfe91363b18bc1d8719381a84c2417
Augusto Peres <augustoapg@gmail.com>
2020-05-28 18:18:08 -0400

    added some validation to the fields

--- src/app/new-transaction/new-transaction.component.ts
+++ src/app/new-transaction/new-transaction.component.ts
@@ -9,16 +9,17 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 export class NewTransactionComponent implements OnInit {
   newTransactionForm;
   types: string[] = ['Expense', 'Income', 'Savings'];
+  chosenType: string = this.types[0]; // default selected
   people: string[] = ['Augusto', 'Camila', 'Both'];
   categories: string[] = ['Home', 'Car', 'Entertainment', 'Utility'];
 
   constructor(private formBuilder: FormBuilder) {
     this.newTransactionForm = new FormGroup({
-      type: new FormControl(''),
-      who: new FormControl(''),
-      category: new FormControl(''),
-      title: new FormControl(''),
-      date: new FormControl(''),
+      type: new FormControl('', Validators.required),
+      who: new FormControl('', Validators.required),
+      category: new FormControl('', Validators.required),
+      title: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.pattern('^\d{1,2}\/\d{1,2}\/\d{4}$')),
       value: new FormControl(''),
       notes: new FormControl(''),
     });
@@ -29,6 +30,8 @@ export class NewTransactionComponent implements OnInit {
 
   onSubmit(transactionData) {
     this.newTransactionForm.reset();
+
+    // TODO: Reset validation warnings after reset!
 
     console.log('Transaction submitted!', transactionData)
   }
