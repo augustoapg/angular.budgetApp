fa2eb26bac0a18cc09a53618ff683acdab01da30
Augusto Peres <augustoapg@gmail.com>
2020-05-29 18:58:13 -0400

    added basic validation to all fields

--- src/app/new-transaction/new-transaction.component.ts
+++ src/app/new-transaction/new-transaction.component.ts
@@ -8,6 +8,15 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class NewTransactionComponent implements OnInit {
   newTransactionForm;
+
+  type: FormControl =  new FormControl('', Validators.required);
+  who: FormControl =  new FormControl('', Validators.required);
+  category: FormControl =  new FormControl('', Validators.required);
+  title: FormControl =  new FormControl('', Validators.required);
+  date: FormControl =  new FormControl('', Validators.required);
+  value: FormControl =  new FormControl('');
+  notes: FormControl =  new FormControl('');
+
   types: string[] = ['Expense', 'Income', 'Savings'];
   chosenType: string = this.types[0]; // default selected
   people: string[] = ['Augusto', 'Camila', 'Both'];
@@ -15,13 +24,13 @@ export class NewTransactionComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {
     this.newTransactionForm = new FormGroup({
-      type: new FormControl('', Validators.required),
-      who: new FormControl('', Validators.required),
-      category: new FormControl('', Validators.required),
-      title: new FormControl('', Validators.required),
-      date: new FormControl('', Validators.pattern('^\d{1,2}\/\d{1,2}\/\d{4}$')),
-      value: new FormControl(''),
-      notes: new FormControl(''),
+      type: this.type,
+      who: this.who,
+      category: this.category,
+      title: this.title,
+      date: this.date,
+      value: this.value,
+      notes: this.notes
     });
   }
 
@@ -32,7 +41,25 @@ export class NewTransactionComponent implements OnInit {
     this.newTransactionForm.reset();
 
     // TODO: Reset validation warnings after reset!
+    this.chosenType = 'Expense'; // TODO: FIX THIS!
 
     console.log('Transaction submitted!', transactionData)
+  }
+
+  getErrorMessage(field) {
+    if (field === 'type'){
+      return 'Please select a type.';
+    } else if (field === 'who') {
+      return 'Please select a person.';
+    } else if (field === 'category') {
+      return 'Please select a category';
+    } else if (field === 'title') {
+      return 'Please select a title';
+    } else if (field === 'date') {
+      return 'Please select a date';
+    } else if (field === 'value') {
+      return 'Please select a value';
+    }
+    return 'Other error'
   }
 }
